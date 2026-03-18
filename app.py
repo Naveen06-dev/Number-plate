4ecdae8cfa70937becc6571bba0825ac83aaf55b
Naveen06-dev <naveenkvg8@gmail.com>
2026-03-18 20:39:00 +0530

    updated design

--- app.py
+++ app.py
@@ -206,4 +206,5 @@ def scan():
             return jsonify({"success": False, "error": error})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
